ed35f4904d90e229d3f5a548a0118c443d702936
Nick Enchev <nick@ensoft.io>
2023-09-23 10:12:49 +0300

    Add project files.

---
+++ sdl2-tutorials/01-Window/sdl2-window.cpp
@@ -0,0 +1,61 @@
+﻿#include <SDL.h>
+#include <iostream>
+
+using namespace std;
+
+struct Application
+{
+	SDL_Window *window;
+};
+
+void initializeSDL(Application &app)
+{
+	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	{
+		std::cout << "Couldn't initialize SDL2" << std::endl;
+		exit(1);
+	}
+	app.window = SDL_CreateWindow("SDL2 Tutorials", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 768, SDL_RENDERER_ACCELERATED);
+	if (!app.window)
+	{
+		std::cout << "Couldn't create the application window" << std::endl;
+		exit(1);
+	}
+}
+
+void shutdownSDL(Application &app)
+{
+	SDL_DestroyWindow(app.window);
+	SDL_Quit();
+}
+
+void gameLoop(Application &app)
+{
+	SDL_Event event;
+	bool running = true;
+	while(running)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+				case SDL_QUIT:
+				{
+					running = false;
+				}
+			}
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	Application app;
+	initializeSDL(app);
+
+	gameLoop(app);
+
+	shutdownSDL(app);
+	return 0;
+}
+
